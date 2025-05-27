fa206d58ac5b47b9121bc7cad437ec282fd49746
xPritam07 <pritambbk1982@gmail.com>
2025-05-27 13:06:25 +0530

    Home page HTML 1

--- controllers/routes.py
+++ controllers/routes.py
@@ -2,8 +2,6 @@ from flask import render_template, request, redirect, url_for, session
 from app import app
 
 
-
-
 @app.route('/')
 def home_page():
-    return render_template('home_page.html')
+    return render_template('/Before_login_part/home_page.html')
