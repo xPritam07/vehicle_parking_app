419cf9796617a568ec0a81dd7ea6de7ed5af31a0
xPritam07 <pritambbk1982@gmail.com>
2025-05-29 18:30:49 +0530

    adding pricing page

--- controllers/routes.py
+++ controllers/routes.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/')
 def home_page():
     return render_template('/Before_login_part/home_page.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template("/Before_login_part/pricing_page.html")
