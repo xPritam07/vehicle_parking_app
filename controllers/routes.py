effe596f4edd83095a8cc85a94a841ba28b7bd31
xPritam07 <pritambbk1982@gmail.com>
2025-06-05 23:44:04 +0530

    model writing

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,5 +1,29 @@
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, flash
+from models.models import db, User, ParkingLot, ParkingSpot, Reservation, Doubt
 from app import app
+from functools import wraps
+
+
+def auth_required(func):
+    @wraps(func)
+    def inner(*args,**kwargs):
+        if 'user_id' not in session:
+            flash("Please log in to continue")
+            return redirect(url_for('login_page'))
+        return func(*args,**kwargs)
+    return inner
+
+def admin_required(func):
+    @wraps(func)
+    def inner(*args,**kwargs):
+        if "user_id" not in session:
+            return redirect(url_for('login_page'))
+        user=User.query.get(session['user_id'])
+        if not user.is_admin:
+            flash('You are not a authorized personel')
+            return redirect(url_for('login_page'))
+        return func(*args,**kwargs)
+    return inner
 
 
 @app.route('/')
@@ -14,10 +38,44 @@ def pricing():
 def contact_page():
     return render_template("/Before_login_part/contact.html")
 
-@app.route('/login')
-def login_page():
-    return render_template("/Before_login_part/login.html")
-
 @app.route('/about')
 def about_page():
     return render_template('/Before_login_part/about_page.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'GET':
+        return render_template("/Before_login_part/login.html")
+
+    else:
+        form_type = request.form.get('form-type')
+
+        if form_type == "login":
+            emailId = request.form.get('email')
+            password = request.form.get('password')
+            user = User.query.filter_by(emailId=emailId).first()
+
+            if emailId == '' or password == '':
+                return flash("Email or Password can not be empty.", "danger")
+            if not user:
+                flash("User does not exist.")
+                return redirect(url_for('login_page'))
+            return redirect(url_for('#'))
+        
+        elif form_type == 'register':
+            fullName = request.form.get('fullName')
+            email = request.form.get('email')
+            password = request.form.get('password')
+
+            user = User.query.filter_by(emailId = email).first()
+
+            if user:
+                flash("Email alrady registered!", 'danger')
+                return redirect(url_for('login_page'))
+            
+            newUser = User(fullName = fullName, emailId = email, password = password)
+            db.session.add(newUser)
+            db.session.commit()
+
+            flash('Successful registration!', 'success')
+            return redirect(url_for('login_page'))
---
+++ models/models.py
@@ -0,0 +1,88 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+db = SQLAlchemy(app)
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+class User(db.Model):
+    __tablename__="user"
+    id = db.Column(db.Integer, primary_key=True)
+    fullName = db.Column(db.String(128), nullable = False, unique = False)
+    emailId = db.Column(db.String(128), nullable = False, unique = True)
+    passhash = db.Column(db.String(128), nullable = False, unique = False)
+    is_admin = db.Column(db.Boolean, nullable = False, default = False)
+
+    reservation = db.relationship('Reservation', cascade = "all, delete", passive_deletes = True, back_populates = 'user')
+
+
+    @property
+    def password(self):
+        raise AttributeError('Password is secret')
+
+    @password.setter
+    def password(self, password):
+        self.passhash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.passhash, password)
+
+class ParkingLot(db.Model):
+    __tablename__ = "parkinglot"
+    id = db.Column(db.Integer, primary_key = True)
+    locationName = db.Column(db.String(128))
+    address = db.Column(db.String(512), unique = True, nullable = False)
+    pincode = db.Column(db.String(128), unique = True, nullable = False)
+    parkLiteCount = db.Column(db.Integer)
+    parkSmartCount = db.Column(db.Integer)
+    parkProCount = db.Column(db.Integer)
+    ratings = db.Column(db.Integer)
+
+    parkingspot = db.relationship('ParkingSpot', cascade = "all, delete", passive_deletes = True, back_populates = 'parkinglot')
+
+class ParkingSpot(db.Model):
+    __tablename__ = "parkingspot"
+    id = db.Column(db.Integer, primary_key = True)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parkinglot.id', ondelete = 'CASCADE'), nullable = False)
+    type = db.Column(db.Integer, nullable = False)
+
+    parkinglot = db.relationship('ParkingLot', back_populates='parkingspot')
+    reservation = db.relationship('Reservation', cascade = "all, delete", passive_deletes = True, back_populates = 'parkingspot')
+
+
+class Reservation(db.Model):
+    __tablename__ = "reservation"
+    id = db.Column(db.Integer, primary_key = True)
+    spotId = db.Column(db.Integer, db.ForeignKey("parkingspot.id", ondelete = "CASCADE"), nullable = False)
+    userId = db.Column(db.Integer, db.ForeignKey("user.id", ondelete = "CASCADE"),  nullable = False)
+    parkingTimestamp = db.Column(db.String(128))
+    leavingTimestamp = db.Column(db.String(128))
+    parkingCost = db.Column(db.Integer)
+    ratings = db.Column(db.Integer)
+
+    user = db.relationship('User', back_populates='reservation')
+    parkingspot = db.relationship('ParkingSpot', back_populates='reservation')
+
+
+class Doubt(db.Model):
+    __tablename__ = "doubts"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(128), nullable = False)
+    email = db.Column(db.String(128), nullable = False)
+    question = db.Column(db.String(512), nullable = False)
+
+
+with app.app_context(): 
+    db.create_all()
+    admin = User.query.filter_by(emailId='admin@gmail.com').first()
+    if not admin:
+        admin=User(emailId='admin@gmail.com',password='admin',fullName='admin',is_admin=True)
+        db.session.add(admin)
+        db.session.commit()
